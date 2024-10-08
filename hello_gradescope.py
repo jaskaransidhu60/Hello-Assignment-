a7f66afbdbebe4a85310cbdd6b0898ac89c9db9a
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-10-07 18:40:46 -0700

    Create hello_gradescope.py

---
+++ hello_gradescope.py
@@ -0,0 +1,9 @@
+# Name: Jaskaran Singh Sidhu
+# OSU Email: sidhuja@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: Assignment 0: Hello World Submission
+# Due Date: Monday, 7 OCT
+# Description: This function is a simple test for submission to Gradescope. It returns the integer 42.
+
+def gradescope_test():
+    return 42
